465cdab5e68fcab71a37cd7bb01699eba4ab3345
coimuy <114358164+coimuy@users.noreply.github.com>
2022-09-25 20:50:39 -0300

    Add files via upload

---
+++ robo_navegador.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+navegador = webdriver.Chrome(executable_path=r'./chromedriver')
+navegador.get('https://best.aliexpress.com')
+navegador.find_element(By.ID,'search-key').send_keys('redmi')
+navegador.find_element(By.CLASS_NAME,'search-button').click()
+#navegador.find_element(By.TAG_NAME,'a')
+
+
