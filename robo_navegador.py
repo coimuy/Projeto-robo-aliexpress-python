38b10a5131da2187638b8e199945a413f65e1a4b
Matheus Martins Sena <matheussena311@gmail.com>
2022-09-26 17:11:25 -0300

    Adicionando ao codigo a capacidade de amazenar os links dos produtos

--- robo_navegador.py
+++ robo_navegador.py
@@ -5,6 +5,11 @@ navegador = webdriver.Chrome(executable_path=r'./chromedriver')
 navegador.get('https://best.aliexpress.com')
 navegador.find_element(By.ID,'search-key').send_keys('redmi')
 navegador.find_element(By.CLASS_NAME,'search-button').click()
-navegador.find_element(By.TAG_NAME,'a')
-navegador.find_element(By.TAG_NAME,'l1')
-
+while x < 10:
+    x += 1
+    print (x)
+    #print(type(x))
+    Link_produtos = navegador.find_element(By.XPATH,"/html/body/div[3]/div/div/div[2]/div[2]/div/div[2]/a[{}]".format(x)).get_attribute('href')
+    print(Link_produtos)
+    Vetor_Links.append(Link_produtos)
+#print (Vetor_Links)
