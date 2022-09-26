5cd97b4aed70d6248f2e193a54d3e37ba00f6a45
Matheus Martins Sena <matheussena311@gmail.com>
2022-09-25 21:24:31 -0300

    Adicionando a procura da classe l1

--- robo_navegador.py
+++ robo_navegador.py
@@ -5,6 +5,6 @@ navegador = webdriver.Chrome(executable_path=r'./chromedriver')
 navegador.get('https://best.aliexpress.com')
 navegador.find_element(By.ID,'search-key').send_keys('redmi')
 navegador.find_element(By.CLASS_NAME,'search-button').click()
-#navegador.find_element(By.TAG_NAME,'a')
+navegador.find_element(By.TAG_NAME,'a')
 
 
