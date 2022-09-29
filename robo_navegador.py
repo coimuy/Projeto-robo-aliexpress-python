55f9501c8c204f5c8246becd6379f1bb2726375b
Matheus Martins Sena <matheussena311@gmail.com>
2022-09-29 14:57:16 -0300

    Arrumando algumas linhas de codigo

--- robo_navegador.py
+++ robo_navegador.py
@@ -1,15 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-x = 1
-vectorLinks = []
-browser = webdriver.Chrome(executable_path=r'./chromedriver')
-browser.get('https://best.aliexpress.com')
-browser.find_element(By.ID,'search-key').send_keys('redmi')
-browser.find_element(By.CLASS_NAME,'search-button').click()
-while x <= 10:
-    x += 1
-    print (x)
-    linkProduct = browser.find_element(By.XPATH,"/html/body/div[3]/div/div/div[2]/div[2]/div/div[2]/a[{}]".format(x)).get_attribute('href')
-    print(linkProduct)
-    vectorLinks.append(linkProduct)
 
+
+def main():
+    x = 1
+    vectorLinks = []
+    browser = webdriver.Chrome(executable_path=r'./chromedriver')
+    browser.get('https://best.aliexpress.com')
+    browser.find_element(By.ID,'search-key').send_keys('redmi')
+    browser.find_element(By.CLASS_NAME,'search-button').click()
+
+    while x <= 10:
+        x += 1
+        print (x)
+        linkProduct = browser.find_element(By.XPATH,
+                                           "/html/body/div[3]/div/div/div[2]/div[2]/div/div[2]/a[{}]".format(
+                                               x)).get_attribute('href')
+        print(linkProduct)
+        vectorLinks.append(linkProduct)
+
+
+if __name__ == "__main__":
+    main()
