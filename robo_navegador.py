9cec0159c32bbb3a17beb6f66927c6e0a4f33361
Matheus Martins Sena <matheussena311@gmail.com>
2022-09-29 14:42:22 -0300

    trocando o nome das variaveis para do portugues para o ingles

--- robo_navegador.py
+++ robo_navegador.py
@@ -1,16 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-x = 0
-Vetor_Links = []
-navegador = webdriver.Chrome(executable_path=r'./chromedriver')
-navegador.get('https://best.aliexpress.com')
-navegador.find_element(By.ID,'search-key').send_keys('redmi')
-navegador.find_element(By.CLASS_NAME,'search-button').click()
-while x < 10:
+x = 1
+vectorLinks = []
+browser = webdriver.Chrome(executable_path=r'./chromedriver')
+browser.get('https://best.aliexpress.com')
+browser.find_element(By.ID,'search-key').send_keys('redmi')
+browser.find_element(By.CLASS_NAME,'search-button').click()
+while x <= 10:
     x += 1
     print (x)
-    #print(type(x))
-    Link_produtos = navegador.find_element(By.XPATH,"/html/body/div[3]/div/div/div[2]/div[2]/div/div[2]/a[{}]".format(x)).get_attribute('href')
-    print(Link_produtos)
-    Vetor_Links.append(Link_produtos)
-#print (Vetor_Links)
+    linkProduct = browser.find_element(By.XPATH,"/html/body/div[3]/div/div/div[2]/div[2]/div/div[2]/a[{}]".format(x)).get_attribute('href')
+    print(linkProduct)
+    vectorLinks.append(linkProduct)
+
