cb5290b4cdac0dd0a3127879804c74dcfe4b4a06
Matheus Martins Sena <matheussena311@gmail.com>
2022-09-29 17:19:05 -0300

    Agora o codigo é capaz de amazenar tanto o nome e preço dos 10 primeiros produtos da tabela

--- robo_navegador.py
+++ robo_navegador.py
@@ -5,20 +5,29 @@ from selenium.webdriver.common.by import By
 def main():
     x = 1
     vectorLinks = []
+    vectorName = []
+    vectorPrice = []
     browser = webdriver.Chrome(executable_path=r'./chromedriver')
     browser.get('https://best.aliexpress.com')
     browser.find_element(By.ID,'search-key').send_keys('redmi')
     browser.find_element(By.CLASS_NAME,'search-button').click()
 
     while x <= 10:
-        x += 1
         print (x)
         linkProduct = browser.find_element(By.XPATH,
                                            "/html/body/div[3]/div/div/div[2]/div[2]/div/div[2]/a[{}]".format(
                                                x)).get_attribute('href')
-        print(linkProduct)
+        nameProduct = browser.find_element(By.XPATH,
+                                           '//*[@id="root"]/div/div/div[2]/div[2]/div/div[2]/a[{}]/div[2]/div[1]'.format(
+                                               x)).text
+        priceProduct = browser.find_element(By.XPATH,
+                                            '//*[@id="root"]/div/div/div[2]/div[2]/div/div[2]/a[{}]/div[2]/div[2]'.format(
+                                               x)).text
         vectorLinks.append(linkProduct)
-
-
+        vectorName.append(nameProduct)
+        vectorPrice.append(priceProduct)
+        x += 1
+    
+    
 if __name__ == "__main__":
     main()
